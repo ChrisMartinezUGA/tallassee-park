a5cedab2ae402dc61db5a8d0745d1a661228c799
ChrisMartinezUGA <33460358+ChrisMartinezUGA@users.noreply.github.com>
2020-02-13 10:09:35 -0500

    Integrating a Navigation Controller

--- tallassee-mobile/TallasseePark/App.js
+++ tallassee-mobile/TallasseePark/App.js
@@ -1,12 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -19,15 +14,9 @@ import {
   Image
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+function HomeScreen({ navigation }) {
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -41,36 +30,59 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
-          <View style={styles.sectionContainer}>
-              <Image style={{width: 300, height: 100}} source={require('./data/images/oconee-river-land-trust.png')}></Image>
-              </View>
+            <View style={styles.sectionContainer}>
+              <Image style={{ width: 300, height: 100 }} source={require('./data/images/oconee-river-land-trust.png')}></Image>
+            </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Tallasee Park</Text>
-              <Text style={styles.sectionDescription}>
-                Subtitle
-              </Text>
+              <Text style={styles.sectionDescription}>Subtitle</Text>
             </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Password</Text>
               <TextInput secureTextEntry={true} style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}>Password</TextInput>
             </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Disclaimer</Text>
               <Text style={styles.sectionDescription}>
-                Tallasee Park is not open to the public. 
-                This land is under preservation and is only open to approved land trust employees. 
+                Tallasee Park is not open to the public.
+                This land is under preservation and is only open to approved land trust employees.
                 This app is solely for testing new activities and should only be accessed by approved users.
               </Text>
             </View>
+
             <View style={styles.sectionContainer}>
-              <Button title="Login"></Button>
+              <Button title="Login" onPress={() => navigation.navigate('Details')}></Button>
             </View>
           </View>
         </ScrollView>
       </SafeAreaView>
     </>
   );
-};
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   scrollView: {
