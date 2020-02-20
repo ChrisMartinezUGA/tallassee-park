52b3bcd50ad83b4b28a0135653763bc531d3fd5d
ChrisMartinezUGA <33460358+ChrisMartinezUGA@users.noreply.github.com>
2020-02-20 13:09:28 -0500

    Progress Bar concept screen
    Requires: $npm install -> $cd ios -> $pod install

--- tallassee-mobile/TallasseePark/data/screens/Progress.js
+++ tallassee-mobile/TallasseePark/data/screens/Progress.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native';
+import * as Progress from 'react-native-progress';
+import MainStyle from '../styles/MainStyle';
+
+const styles = MainStyle;
 
 function ProgressScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Progress Screen</Text>
-    </View>
+    <>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Progress.Bar progress={0.3} width={200} height={25} />
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Current Rank</Text>
+              <Text style={styles.sectionDescription}>Tadpole</Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Badges</Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={{ width: 100, height: 100}} source={require('./../../data/images/star.png')} />
+                <Image style={{ width: 100, height: 100}} source={require('./../../data/images/tree.png')} />
+                <Image style={{ width: 100, height: 100}} source={require('./../../data/images/river.png')} />
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
 
