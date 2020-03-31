54d270a80f5e827d04940c50911ba029f158b636
ChrisMartinezUGA <33460358+ChrisMartinezUGA@users.noreply.github.com>
2020-03-31 18:19:44 -0400

    Added map layer float action

--- tallassee-mobile/TallasseePark/data/screens/Map.js
+++ tallassee-mobile/TallasseePark/data/screens/Map.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import { ThemeProvider, Button } from 'react-native-elements';
 import { FloatingAction } from "react-native-floating-action";
-import ImageZoom from 'react-native-image-pan-zoom';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import {
-  View,
-  Text,
-  Image,
-  Dimensions,
-  StatusBar
-} from 'react-native';
+import { View, Image, StatusBar } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const actions = [
   {
@@ -36,40 +28,70 @@ const actions = [
   },
 ];
 
-const images = [{
-  url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460'
-}, {
-  url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460'
-}, {
-  url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460'
-}]
+const layers = [
+  {
+    text: "Satellite",
+    icon: require('../icons/running.png'),
+    name: "bt_satellite",
+    position: 1,
+    color: "#363C24"
+  },
+  {
+    text: "Features",
+    icon: require('../icons/compass.png'),
+    name: "bt_features",
+    position: 2,
+    color: "#363C24",
+  },
+  {
+    text: "Topo",
+    icon: require('../icons/info.png'),
+    name: "bt_topo",
+    position: 3,
+    color: "#363C24",
+  },
+];
+
+const images = [ require('../images/aerial-map.png'), require('../images/features-map.png'), require('../images/topo-map.png') ]
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mapIndex: 0,
+      navigation: this.props.navigation
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar barStyle="light-content" />
+        <ScrollView minimumZoomScale={1} maximumZoomScale={5} showsHorizontalScrollIndicator={true} style={{ flex: 1 }} >
+          <Image source={images[this.state.mapIndex]} />
+        </ScrollView>
+
+        <FloatingAction actions={actions} onPressItem={
+          name => {
+            if (name == "bt_activities") { this.state.navigation.navigate('Activities') }
+            else if (name == "bt_explore") { this.state.navigation.navigate('Explore') }
+            else if (name == "bt_info") { this.state.navigation.navigate('Info') }
+          }
+        } color="#363C24" />
+
+        <FloatingAction actions={layers} onPressItem={
+          name => {
+            if (name == "bt_satellite") { this.setState({mapIndex: 0}) }
+            else if (name == "bt_features") { this.setState({mapIndex: 1}) }
+            else if (name == "bt_topo") { this.setState({mapIndex: 2}) }
+          }
+        } color="#363C24" position="left" />
+      </View>
+    )
+  }
+}
 
 function MapScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar barStyle="light-content" />
-      <ScrollView minimumZoomScale={1} maximumZoomScale={5} showsHorizontalScrollIndicator={true} style={{ flex: 1 }} >
-        <Image source={require('../images/topo-map.png')} />
-      </ScrollView>
-      <FloatingAction actions={actions} onPressItem={
-        name => {
-          if (name == "bt_activities") { navigation.navigate('Activities') }
-          else if (name == "bt_explore") { navigation.navigate('Explore') }
-          else if (name == "bt_info") { navigation.navigate('Info') }
-        }
-      } color="#363C24" />
-    </View>
-  );
+  return <Map navigation={navigation} />
 }
-/*
-  <ScrollView minimumZoomScale={1} maximumZoomScale={5} showsHorizontalScrollIndicator={true} style={{flex: 1}} >
-    <Image source={require('../images/topo-map.png')} />
-  </ScrollView>
-
-          <ImageZoom cropWidth={Dimensions.get('window').width}
-        cropHeight={Dimensions.get('window').height}>
-        <Image style={{width:200, height:200}}
-        source={require('../images/topo-map.png')}/>
-      </ImageZoom>
-*/
 export default MapScreen;
