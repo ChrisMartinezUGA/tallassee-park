9028fc3e50b24fbe74d8da3498958327a34c2f93
Jack Towery <jacktowery@gmail.com>
2020-04-01 14:00:31 -0400

    Admin - Explore content management operations
    +Add, Delete, Edit operatons for Explore data

---
+++ tallassee-admin/public.sh
@@ -0,0 +1,9 @@
+rm -rf public/css
+cp -r css public/css
+rm -rf public/js
+cp -r js public/js
+rm -rf public/assets
+cp -r assets public/assets
+cp index.html public/index.html
+cp explore.html public/explore.html
+cp README.md public/README.md
