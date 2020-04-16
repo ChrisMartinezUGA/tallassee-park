fd2f39dd59dc733e8d50706c8b8a3e1b0f2f8454
Jack Towery <jacktowery@gmail.com>
2020-04-16 10:48:16 -0400

    Admin - Published, added favicons

--- tallassee-admin/public.sh
+++ tallassee-admin/public.sh
@@ -3,6 +3,7 @@ mkdir public
 cp -r css public/css
 cp -r js public/js
 cp -r assets public/assets
+cp -r favicons public/favicons
 cp index.html public/index.html
 cp info.html public/info.html
 cp explore.html public/explore.html
