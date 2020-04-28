f8c7ab249a256ba3a4941c4eba1dad26b529ad78
Jack Towery <jacktowery@gmail.com>
2020-04-28 13:41:04 -0400

    Admin - Public push bug (fixed)

--- tallassee-admin/public.sh
+++ tallassee-admin/public.sh
@@ -5,6 +5,7 @@ cp -r js public/js
 cp -r assets public/assets
 cp -r favicons public/favicons
 cp index.html public/index.html
+cp maps.html public/maps.html
 cp info.html public/info.html
 cp explore.html public/explore.html
 cp activities.html public/activities.html
